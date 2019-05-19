1ddbd6aa17f6bf82a8643ad3ff16b0bedc9ea024
David Moreno <d.moreno@mytaxi.com>
2019-05-19 12:23:27 +0200

    Fixed solution

---
+++ src/main/java/com/myapps/robot/exceptions/InvalidInstructionsException.java
@@ -0,0 +1,7 @@
+package com.myapps.robot.exceptions;
+
+public class InvalidInstructionsException extends RuntimeException {
+    public InvalidInstructionsException(String message) {
+        super(message);
+    }
+}
